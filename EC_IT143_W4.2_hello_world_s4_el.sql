a464b06609019ea36bc16fea427f1f1458c683be
Erick Leon <erickleon420@outlook.com>
2024-12-16 22:56:45 -0600

    Add files via upload

---
+++ EC_IT143_W4.2_hello_world_s4_el.sql
@@ -0,0 +1,27 @@
+DROP VIEW IF EXISTS dbo.v_hello_world_load;
+GO
+
+CREATE VIEW dbo.v_hello_world_load
+AS
+
+/*****************************************************************************************************************
+NAME:    dbo.v_hello_world_load;
+PURPOSE: Create the Hello World - Load View 
+
+MODIFICATION LOG:
+Ver      Date        Author        Description
+-----   ----------   -----------   -------------------------------------------------------------------------------
+1.0     05/23/2022   JJAUSSI       1. Built this script for EC IT443
+
+
+RUNTIME: 
+1s
+
+NOTES: 
+This script exists to help me to lear step 4 of 8 in the Answer Focused Approach for T-SQL Data Manipulation
+ 
+******************************************************************************************************************/
+
+
+SELECT 'Hello World' AS my_message
+     , GETDATE() AS current_date_time;
